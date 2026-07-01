47892267c70d6ea9f4bff6003a70dd6decd2f0e2
jiangyi15 <jiangyi15@mails.ucas.ac.cn>
2026-07-01 16:56:24 +0800

    CUDA backends: remove prepare_phsp_batched/free_phsp_batched (no longer used)

--- src/ampfit/backends/cuda_backends.py
+++ src/ampfit/backends/cuda_backends.py
@@ -10,17 +10,10 @@ class CUDABackendV2(ComputeBackend):
     def __init__(self, kernel_config, batch_size=50000):
         from ampfit._cuda_v2 import CUDAKernelV2 as _K
         self.kernel = _K(kernel_config, batch_size=batch_size)
-        self._phsp_np = None; self._phsp_scratch = None; self._phsp_n = 0
     def load_data(self, data_np): return self.kernel.load_data(data_np)
     def compute(self, params, data_handle, norm=None, return_p=True):
         return self.kernel.compute(params, data_handle, norm=norm)
-    def prepare_phsp_batched(self, phsp_np, n_events):
-        self._phsp_np = phsp_np; self._phsp_n = n_events
-        self._phsp_scratch = self.kernel.load_data(phsp_np)
-    def free_phsp_batched(self):
-        self._phsp_np = None
-        if self._phsp_scratch is not None: self._phsp_scratch.free(); self._phsp_scratch = None
-    def free(self): self.free_phsp_batched(); self.kernel.free()
+    def free(self): self.kernel.free()
 
 
 @register_backend("cuda32_v2")
@@ -30,17 +23,10 @@ class CUDABackendV2F32(ComputeBackend):
     def __init__(self, kernel_config, batch_size=50000):
         from ampfit._cuda_v2_f32 import CUDAKernelV2F32 as _K
         self.kernel = _K(kernel_config, batch_size=batch_size)
-        self._phsp_np = None; self._phsp_scratch = None; self._phsp_n = 0
     def load_data(self, data_np): return self.kernel.load_data(data_np)
     def compute(self, params, data_handle, norm=None, return_p=True):
         return self.kernel.compute(params, data_handle, norm=norm)
-    def prepare_phsp_batched(self, phsp_np, n_events):
-        self._phsp_np = phsp_np; self._phsp_n = n_events
-        self._phsp_scratch = self.kernel.load_data(phsp_np)
-    def free_phsp_batched(self):
-        self._phsp_np = None
-        if self._phsp_scratch is not None: self._phsp_scratch.free(); self._phsp_scratch = None
-    def free(self): self.free_phsp_batched(); self.kernel.free()
+    def free(self): self.kernel.free()
 
 
 @register_backend("cuda")
@@ -52,17 +38,10 @@ class CUDABackendV3(ComputeBackend):
     def __init__(self, kernel_config, batch_size=50000):
         from ampfit._cuda_v3 import CUDAKernelV3 as _K
         self.kernel = _K(kernel_config, batch_size=batch_size)
-        self._phsp_np = None; self._phsp_scratch = None; self._phsp_n = 0
     def load_data(self, data_np): return self.kernel.load_data(data_np)
     def compute(self, params, data_handle, norm=None, return_p=True):
         return self.kernel.compute(params, data_handle, norm=norm)
-    def prepare_phsp_batched(self, phsp_np, n_events):
-        self._phsp_np = phsp_np; self._phsp_n = n_events
-        self._phsp_scratch = self.kernel.load_data(phsp_np)
-    def free_phsp_batched(self):
-        self._phsp_np = None
-        if self._phsp_scratch is not None: self._phsp_scratch.free(); self._phsp_scratch = None
-    def free(self): self.free_phsp_batched(); self.kernel.free()
+    def free(self): self.kernel.free()
 
 
 @register_backend("cuda32_v3")
@@ -72,17 +51,10 @@ class CUDABackendV3F32(ComputeBackend):
     def __init__(self, kernel_config, batch_size=50000):
         from ampfit._cuda_v3_f32 import CUDAKernelV3F32 as _K
         self.kernel = _K(kernel_config, batch_size=batch_size)
-        self._phsp_np = None; self._phsp_scratch = None; self._phsp_n = 0
     def load_data(self, data_np): return self.kernel.load_data(data_np)
     def compute(self, params, data_handle, norm=None, return_p=True):
         return self.kernel.compute(params, data_handle, norm=norm)
-    def prepare_phsp_batched(self, phsp_np, n_events):
-        self._phsp_np = phsp_np; self._phsp_n = n_events
-        self._phsp_scratch = self.kernel.load_data(phsp_np)
-    def free_phsp_batched(self):
-        self._phsp_np = None
-        if self._phsp_scratch is not None: self._phsp_scratch.free(); self._phsp_scratch = None
-    def free(self): self.free_phsp_batched(); self.kernel.free()
+    def free(self): self.kernel.free()
 
 
 @register_backend("cuda_mixed_v3")
@@ -91,14 +63,7 @@ class CUDABackendV3Mixed(ComputeBackend):
     def __init__(self, kernel_config, batch_size=50000):
         from ampfit._cuda_v3_mixed import CUDAKernelV3Mixed as _K
         self.kernel = _K(kernel_config, batch_size=batch_size)
-        self._phsp_np = None; self._phsp_scratch = None; self._phsp_n = 0
     def load_data(self, data_np): return self.kernel.load_data(data_np)
     def compute(self, params, data_handle, norm=None, return_p=True):
         return self.kernel.compute(params, data_handle, norm=norm)
-    def prepare_phsp_batched(self, phsp_np, n_events):
-        self._phsp_np = phsp_np; self._phsp_n = n_events
-        self._phsp_scratch = self.kernel.load_data(phsp_np)
-    def free_phsp_batched(self):
-        self._phsp_np = None
-        if self._phsp_scratch is not None: self._phsp_scratch.free(); self._phsp_scratch = None
-    def free(self): self.free_phsp_batched(); self.kernel.free()
+    def free(self): self.kernel.free()
